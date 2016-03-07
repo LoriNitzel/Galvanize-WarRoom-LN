9aaa85c2495c1472efbb6c366f59222ed6b8d59e
Lori Nitzel <lorinitzel@mac.com>
2016-03-07 14:39:37 -0700

    first route working

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-const warroom = require("./warroom-client")
-warroom((error, data) => console.log(data));
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 // var knex = require('../db/knex');
 var router = express.Router();
+const warroom = require("../warroom-client")
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  warroom((error, data) => res.json(data));
 });
+
+router.get('/')
+
 
 module.exports = router;
